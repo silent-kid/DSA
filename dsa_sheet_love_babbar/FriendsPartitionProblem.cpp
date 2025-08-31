a3f733ee22927ca4c01f9a25ddc4e4c2c1e46bf7
silent-kid <jawwadm0000@gmail.com>
2025-08-31 18:00:51 +0530

    LCS

---
+++ dsa_sheet_love_babbar/FriendsPartitionProblem.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countFriendsPairings(int n) {
+        // code here
+        vector<long long> dp(n+1,0);
+        dp[1]=1;
+        dp[2]=2;
+        
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1] + (long long)(i-1)*dp[i-2];
+        }
+        
+        return dp[n];
+    }
+
+int main(){
+    
+    return 0;
+}
---
+++ dsa_sheet_love_babbar/LongestCommonSubsequence.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lcs(string &s1, string &s2) {
+        // code here
+        
+        int m = s1.size();
+        int n = s2.size();
+        
+        vector<vector<int>> t(m + 1, vector<int>(n + 1, 0));
+
+        
+        // for(int i=0;i<=m;i++){
+        //     for(int j=0;j<=n;j++){
+        //         t[i][j]=0;
+        //     }
+        // }
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1[i-1]==s2[j-1]){
+                    t[i][j] = 1 + t[i-1][j-1];
+                }
+                else{
+                    t[i][j] = max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        
+        return t[m][n];
+    }
+
+int main(){
+    
+    return 0;
+}
